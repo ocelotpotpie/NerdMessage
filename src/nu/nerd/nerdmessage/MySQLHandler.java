bcad47800643ada7f55f2cf8e223cbcfd4d49d2c
totemo <TotemoResearch@gmail.com>
2020-03-24 21:26:12 +1030

    Version 1.3.1
    Forcibly load the JDBC driver, resolving database errors in
    some configurations.

--- src/nu/nerd/nerdmessage/MySQLHandler.java
+++ src/nu/nerd/nerdmessage/MySQLHandler.java
@@ -1,40 +1,41 @@
 package nu.nerd.nerdmessage;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.bukkit.configuration.file.FileConfiguration;
 
 import com.avaje.ebean.EbeanServer;
 import com.avaje.ebean.EbeanServerFactory;
 import com.avaje.ebean.config.ServerConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import com.zaxxer.hikari.pool.PoolInitializationException;
+
 import nu.nerd.nerdmessage.mail.MailMessage;
 import nu.nerd.nerdmessage.mail.MailUser;
-import org.bukkit.configuration.file.FileConfiguration;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
 
 public class MySQLHandler {
-
 
     private NerdMessage plugin;
     private final HikariDataSource dataSource;
     private EbeanServer ebeanServer;
 
-
     public MySQLHandler(NerdMessage plugin, FileConfiguration config) {
         this.plugin = plugin;
         dataSource = new HikariDataSource();
-        if (!plugin.getConfig().getBoolean("mysql.enabled")) return;
+        if (!plugin.getConfig().getBoolean("mysql.enabled"))
+            return;
         try {
+            // Load the JDBC driver. See:
+            // https://stackoverflow.com/questions/5556664/how-to-fix-no-suitable-driver-found-for-jdbcmysql-localhost-dbname-error-w/19259608#19259608
+            Class.forName("com.mysql.jdbc.Driver");
             setUpDataSource(config);
             setUpEbean();
-        } catch (PoolInitializationException ex) {
+        } catch (PoolInitializationException | ClassNotFoundException ex) {
             plugin.getConfig().set("mysql.enabled", false);
-            plugin.getLogger().warning("Could not connect to database. Mail will be disabled.");
+            plugin.getLogger().warning("Could not connect to database. Mail will be disabled. Reason: " + ex.getMessage());
         }
     }
-
 
     /**
      * Get the eBean handle
@@ -43,7 +44,6 @@ public class MySQLHandler {
         return ebeanServer;
     }
 
-
     /**
      * Shut down the MySQL pool
      */
@@ -51,9 +51,9 @@ public class MySQLHandler {
         dataSource.close();
     }
 
-
     /**
      * Get a connection from the MySQL pool
+     * 
      * @return connection
      * @throws SQLException
      */
@@ -68,7 +68,6 @@ public class MySQLHandler {
         }
     }
 
-
     /**
      * Set up the MySQL connection pool
      */
@@ -80,7 +79,6 @@ public class MySQLHandler {
         dataSource.setPoolName("NerdMessage");
         dataSource.addDataSourceProperty("cachePrepStmts", "true");
     }
-
 
     /**
      * Set up the eBean ORM
@@ -99,6 +97,5 @@ public class MySQLHandler {
         ebeanServer = EbeanServerFactory.create(config);
         Thread.currentThread().setContextClassLoader(previousCL);
     }
-
 
 }
